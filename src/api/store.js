58df314cbae5856e83ea39020e8cd0eeed98841d
Isa Phuyuthanon <uronohan@gmail.com>
2020-07-16 18:38:53 +0700

    adding redux store

---
+++ src/api/store.js
@@ -0,0 +1,20 @@
+import { createStore, combineReducers } from 'redux';
+
+// Reducer
+const auth = (state = { user: null}, action ) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return { ...state, user: action.payload.user };
+    case 'LOGOUT':
+      localStorage.clear();
+      return {...state,  user: null};
+    default:
+      return state;
+  }
+};
+
+
+// Store
+export const store = createStore(
+  combineReducers(auth)
+);
